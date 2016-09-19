41bff133895a4a690b24a5310a62f7c8ca3171c4
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2016-09-18 20:14:32 -0700

    Use simplifier object to keep track of steps
    TODO: eliminate pointless setCompare proc
    TODO: eliminate useless substeps in a more robust way

--- boolalg.nim
+++ boolalg.nim
@@ -1,4 +1,6 @@
 import sequtils
+import streams
+import strutils
 
 type
   BVar = distinct string
@@ -35,6 +37,54 @@ proc `-`*(exp: BExp): BExp = BExp(kind: BENot, exp: exp)
 proc `$`*(exp: BExp): string
 proc `==`*(lhs, rhs: BExp): bool
 
+proc addStep(simpl: BExpSimplifier, next: BExp, comment: string)
+proc newBExpSimplifier(exp: BExp): BExpSimplifier =
+  new result
+  newSeq(result.history, 0)
+  result.history.add( (exp, "Start", nil) )
+  result.current = exp
+
+proc addStep(simpl: BExpSimplifier, next: BExp, comment: string) =
+  for index, historyItem in simpl.history:
+    if not isNil(historyItem.res):
+      if historyItem.res == next:
+        return
+  simpl.history.add( (next, comment, nil) )
+  simpl.current = next
+
+proc addSubstep(simpl: BExpSimplifier, subSimpl: BExpSimplifier, comment: string) =
+  simpl.history.add( (nil, comment, subSimpl) )
+
+proc addSubstep(simpl: BExpSimplifier, subStepStart: BExp, comment: string): BExpSimplifier =
+  let subSimpl = newBExpSimplifier(subStepStart)
+  simpl.addSubStep(subSimpl, comment)
+  return subSimpl
+
+proc pruneUselessSteps(simpl: BExpSimplifier) =
+  # Go through and remove any substeps that start and end with the same thing
+  var index = simpl.history.len
+  while index > 0:
+    dec index
+
+    if not isNil(simpl.history[index].substep):
+      let substep = simpl.history[index].substep
+      substep.pruneUselessSteps()
+      if substep.current == substep.history[0].res:
+        simpl.history.delete(index)
+
+
+proc write(s: Stream, simpl: BExpSimplifier, indent = 0) =
+  for step in simpl.history:
+    s.write(repeat(' ', indent))
+    if step.substep.isNil:
+      s.write($step.res)
+      s.write(repeat(' ', 30 - ($step.res).len))
+      s.write(step.comment)
+    else:
+      s.write(step.substep, indent + 1)
+    s.write("\n")
+
+
 proc containsAll[T](s1, s2: openArray[T]): bool =
   for el in s2:
     if el notin s1:
@@ -60,18 +110,21 @@ proc setCompare[T](s1, s2: openArray[T], common, extra: var seq[T]) =
 proc filterFalseTerms(terms: var seq[BExp]) =
   terms.keepItIf(it != BFalse)
 
+proc filterTrueTerms(terms: var seq[BExp]) =
+  terms.keepItIf(it != BTrue)
+
 proc product*(terms: seq[BExp]): BExp =
   if terms.len == 0:
     BTrue
   else:
     foldl(terms, a * b)
-  
+
 proc sum*(terms: seq[BExp]): BExp =
   if terms.len == 0:
     BFalse
   else:
     foldl(terms, a + b)
-  
+
 
 proc collectSumTerms(exp: BExp): seq[BExp] =
   newSeq(result, 0)
@@ -106,10 +159,7 @@ proc `==`*(lhs, rhs: BExp): bool =
       let lhsTerms = collectProc(lhs)
       let rhsTerms = collectProc(rhs)
 
-      if lhsTerms.len != rhsTerms.len:
-        return false
-      
-      return lhsTerms.containsAll(rhsTerms)
+      return lhsTerms.setEquals(rhsTerms)
 
 proc parenthesize(exp: BExp, kinds: set[BExpKind]): string =
   if exp.kind in kinds:
@@ -138,28 +188,46 @@ proc `$`*(exp: BExp): string =
       return str & "'"
 
 # converts POS to SOP
-proc killPOS(exp: BExp): BExp =
+proc killPOS(simpl: BExpSimplifier) =
+  let exp = simpl.current
   case exp.kind:
     of BEVar, BETrue, BEFalse:
-      return exp
+      return
     of BENot:
-      if exp.exp.kind == BESum:
+      if exp.exp.kind in {BESum, BEProd}:
         # Apply DeMorgan's law: (a + b)' = a'b'
-        return killPOS((-killPOS(exp.exp.lhs)) * (-killPOS(exp.exp.rhs)))
-      elif exp.exp.kind == BEProd:
-        # Apply DeMorgan's law: (ab)' = a' + b'
-        return killPOS((-killPOS(exp.exp.lhs)) + (-killPOS(exp.exp.rhs)))
+        let sublhs = simpl.addSubstep(exp.exp.lhs, "Simplify left hand side")
+        let subrhs = simpl.addSubstep(exp.exp.rhs, "Simplify right hand side")
+        sublhs.killPOS()
+        subrhs.killPOS()
+        if exp.exp.kind == BESum:
+          simpl.addStep((-sublhs.current) * (-subrhs.current), "Apply DeMorgan's law: (a + b)' = a'b'")
+        else:
+          simpl.addStep((-sublhs.current) + (-subrhs.current), "Apply DeMorgan's law: (ab)' = a' + b'")
+        simpl.killPOS()
       elif exp.exp.kind == BENot:
-        return killPOS(exp.exp.exp);
+        simpl.addStep(exp.exp.exp, "Eliminate double negative")
+        simpl.killPOS()
       else:
-        return -killPOS(exp.exp)
+        let sub = simpl.addSubstep(exp.exp, "Simplify inside NOT")
+        sub.killPOS()
+        simpl.addStep(-sub.current, "Reapply NOT")
     of BESum:
-      return killPOS(exp.lhs) + killPOS(exp.rhs)
+      let sublhs = simpl.addSubstep(exp.lhs, "Simplify left hand side")
+      let subrhs = simpl.addSubstep(exp.rhs, "Simplify right hand side")
+      sublhs.killPOS()
+      subrhs.killPOS()
+      simpl.addStep(sublhs.current + subrhs.current, "Recombine left hand and right hand sides")
+      return
     of BEProd:
       # This will distribute the product over any sums
 
-      var lhs = killPOS(exp.lhs)
-      var rhs = killPOS(exp.rhs)
+      let sublhs = simpl.addSubstep(exp.lhs, "Simplify left hand side")
+      let subrhs = simpl.addSubstep(exp.rhs, "Simplify right hand side")
+      sublhs.killPOS()
+      subrhs.killPOS()
+      var lhs = sublhs.current
+      var rhs = subrhs.current
       var temp: BExp
 
       if rhs.kind == BESum:
@@ -170,43 +238,73 @@ proc killPOS(exp: BExp): BExp =
         rhs = temp
       else:
         # There's nothing to distribute
-        return lhs * rhs
+        simpl.addStep(lhs * rhs, "Recombine left and right hand sides")
+        return
 
       # now rhs.kind == BESum is guaranteed
       let rrhs = rhs.rhs
       let rlhs = rhs.lhs
 
-      return killPOS(lhs * rlhs) + killPOS(lhs * rrhs)
+      let rrlhs = lhs * rlhs
+      let rrrhs = lhs * rrhs
 
-proc simplifyProduct(exp: BExp): BExp
-proc simplifySum(exp: BExp): BExp
-proc simplifySumStep(exp: BExp): tuple[s: bool, e: BExp]
+      simpl.addStep(rrlhs + rrrhs, "Distribute " & $lhs & " into " & $rhs)
 
-proc simplify*(exp: BExp): BExp =
-  let sopexp = killPOS(exp)
+      let sublhs2 = simpl.addSubstep(rrlhs, "Simplify left hand side")
+      let subrhs2 = simpl.addSubstep(rrrhs, "Simplify right hand side")
+      sublhs2.killPOS()
+      subrhs2.killPOS()
+      simpl.addStep(sublhs2.current + subrhs2.current, "Recombine left and right hand sides")
+
+proc simplifyProduct(simpl: BExpSimplifier)
+proc simplifySum(simpl: BExpSimplifier)
+proc simplifySumStep(simpl: BExpSimplifier): bool
+proc pruneImpliedTerms(simpl: BExpSimplifier)
+
+proc simplify*(simpl: BExpSimplifier, prune = true) =
+  simpl.killPOS()
+
+  let sopexp = simpl.current
 
   case sopexp.kind:
     of BEVar, BETrue, BEFalse:
-      return sopexp
+      discard
     of BENot:
-      return -simplify(sopexp.exp)
+      let subnot = simpl.addSubstep(sopexp.exp, "Simplify under NOT")
+      subnot.simplify()
+      simpl.addStep(-subnot.current, "Put expression back under NOT")
     of BEProd:
-      return simplifyProduct(sopexp)
+      simpl.simplifyProduct()
     of BESum:
-      return simplifySum(sopexp)
+      simpl.simplifySum()
 
-proc simplifySum(exp: BExp): BExp =
+  if prune:
+    simpl.pruneImpliedTerms()
+
+proc simplifySum(simpl: BExpSimplifier) =
+  let exp = simpl.current
+
   # recursively collect terms
   var terms = collectSumTerms(exp)
 
   # first pass: simplify, return true if a term is true, remove false terms
   for index, term in terms:
     # simplify
-    terms[index] = simplify(term)
+    let subterm = simpl.addSubstep(term, "Simplify term of sum")
+    subterm.simplify()
+    terms[index] = subterm.current
+    simpl.addStep(sum(terms), "Recombine simplified term")
+
     if terms[index].kind == BETrue:
-      return term
+      simpl.addStep(term, "If TRUE appears in a sum, whole result is true")
+      return
 
   terms = deduplicate(terms)
+  simpl.addStep(sum(terms), "Remove duplicate terms")
+
+  filterFalseTerms(terms)
+  simpl.addStep(sum(terms), "Remove false terms")
+
 
   # second pass: check for x + x'
   for index1, term1 in terms:
@@ -221,32 +319,41 @@ proc simplifySum(exp: BExp): BExp =
             terms[index1] = BTrue
             terms[index2] = BTrue
 
-  filterFalseTerms(terms)
+
+  filterTrueTerms(terms)
+  var newExp = sum(terms)
+  if newExp == BFalse:
+    newExp = BTrue
+  simpl.addStep(newExp, "Remove a + a' terms")
 
   var success = true
-  var newExp = sum(terms)
 
   while success:
-    (success, newExp) = simplifySumStep(newExp)
-
-  return newExp
+    success = simpl.simplifySumStep()
 
 # IMPORTANT: assumes it's not a POS, rather just product of other products
 # (or just two variables)
-proc simplifyProduct(exp: BExp): BExp =
+proc simplifyProduct(simpl: BExpSimplifier) =
+  let exp = simpl.current
+
   # recursively collect terms
   var terms = collectProdTerms(exp)
 
   # first pass: simplify, return false if a term is false, remove true terms
   for index, term in terms:
     # simplify
-    terms[index] = simplify(term)
+    let subterm = simpl.addSubstep(term, "Simplify term of product")
+    subterm.simplify()
+    terms[index] = subterm.current
+    simpl.addStep(product(terms), "Recombine simplified term")
     if terms[index].kind == BEFalse:
-      return term
+      simpl.addStep(term, "If a product contains FALSE, its value is FALSE")
+      return
     if terms[index].kind == BETrue:
       terms.delete(index)
 
   terms = deduplicate(terms)
+  simpl.addStep(product(terms), "Remove TRUE terms and duplicates from product")
 
   # second pass: check for aa'
   for index1, term1 in terms:
@@ -254,21 +361,22 @@ proc simplifyProduct(exp: BExp): BExp =
       if index1 != index2:
         if term1.kind == BENot:
           if term2 == term1.exp:
-            return BFalse
+            simpl.addStep(BFalse, "If a product has a term and its complement, it is FALSE")
+            return
         if term2.kind == BENot:
           if term1 == term2.exp:
-            return BFalse
+            simpl.addStep(BFalse, "If a product has a term and its complement, it is FALSE")
+            return
 
-  return product(terms)
+  simpl.addStep(product(terms), "Remove terms if their complement is also there")
 
-proc simplifySumStep(exp: BExp): tuple[s: bool, e: BExp] =
-  result.s = false
-  result.e = exp
+proc simplifySumStep(simpl: BExpSimplifier): bool =
+  let exp = simpl.current
 
   if exp.kind != BESum:
-    return
+    return false
 
-  var terms = collectSumTerms(exp).map(simplifyProduct)
+  var terms = collectSumTerms(exp)
   filterFalseTerms(terms)
 
   for index1, term1 in terms:
@@ -289,19 +397,35 @@ proc simplifySumStep(exp: BExp): tuple[s: bool, e: BExp] =
         if commonTerms.len > 0:
           let outside = product(commonTerms)
           let inside = product(terms1) + product(terms2)
-          let insideSimplified = simplify(inside)
+          let subinside = newBExpSimplifier(inside)
+          subinside.simplify()
+          let insideSimplified = subinside.current
 
           if inside == insideSimplified:
             continue
           else:
-            result.s = true
             terms[index1] = BFalse
             terms[index2] = BFalse
-            terms.add(collectSumTerms(simplify(outside * insideSimplified)))
             filterFalseTerms(terms)
+            terms.add(outside * inside)
+            simpl.addStep(sum(terms), "Factor out common terms")
+            simpl.addSubstep(subinside, "Simplify the inside part")
+            discard terms.pop()
 
-            result.e = sum(terms)
-            return
+            terms.add(outside * insideSimplified)
+            simpl.addStep(sum(terms), "Recombine simplified inside")
+
+            let newTerm = terms.pop()
+            let subnt = simpl.addSubstep(newTerm, "Simplify factored term")
+            subnt.simplify()
+            terms.add(collectSumTerms(subnt.current))
+
+
+            # collectSumTerms(simplify(outside * insideSimplified))
+
+            simpl.addStep(sum(terms), "Recombine simplified factored term")
+            return true
+  return false
 
 # constraint is a list of variables (or negative variables)
 # term is also that
@@ -320,38 +444,79 @@ proc checkImplication(constraints, term: seq[BExp]): bool =
   for constraint in constraints:
     result = result or checkConstraint(collectProdTerms(constraint), term)
 
-proc pruneImpliedTerms(exp: BExp): BExp =
+proc collectCommonTerms(terms: seq[seq[BExp]]): seq[BExp] =
+  var common, extra: seq[BExp]
+  newSeq(common, 0)
+  newSeq(extra, 0)
+
+  for index in low(terms)+1..high(terms):
+    let term = terms[index]
+    let prevTerm = terms[index-1]
+    setCompare(term, prevTerm, common, extra)
+
+  return common
+
+proc pruneImpliedTerms(simpl: BExpSimplifier) =
+  let exp = simpl.current
+  if exp.kind != BESum:
+    return
+
   var terms = collectSumTerms(exp)
 
   var termsToDelete: seq[int]
   newSeq(termsToDelete, 0)
 
-  for index, _ in terms:
-    let deletedTerm = terms[index]
+  for index, deleted in terms:
+    let checkTerm = terms[index]
     var termscopy = terms
     termscopy.delete(index)
 
-    let constraintExp = simplify(-sum(termscopy))
+    let simpltc = newBExpSimplifier(-sum(termscopy))
+    simpltc.simplify(false)
+    let constraintExp = simpltc.current
     let constraints = collectSumTerms(constraintExp)
 
-    let deletedTermPTerms = collectProdTerms(deletedTerm)
+    var checkTermPTerms = collectProdTerms(checkTerm)
 
-    if not checkImplication(constraints, deletedTermPTerms):
+    if checkImplication(constraints, checkTermPTerms):
+      # The term can't be deleted but maybe some of its parts can
+      let guaranteedTrue = collectCommonTerms(constraints.map(collectProdTerms) & checkTermPTerms)
+      if guaranteedTrue.len > 0:
+        var index2 = checkTermPTerms.len()
+        while index2 > 0:
+          dec index2
+          if checkTermPTerms[index2] in guaranteedTrue:
+            checkTermPTerms[index2] = BTrue
+
+        let newProduct = product(checkTermPTerms)
+        let prodsimpl = newBExpSimplifier(newProduct)
+        prodsimpl.simplifyProduct()
+        terms[index] = prodsimpl.current
+        simpl.addStep(sum(terms), "Remove terms guaranteed to be true")
+    else:
       termsToDelete.add(index)
-  
+
   var index = termsToDelete.len
   while index > 0:
     dec index
-    terms.delete(termsToDelete[index])
-  
-  return sum(terms)
+    let indexToDelete = termsToDelete[index]
+    let deletedTerm = terms[indexToDelete]
+    terms.delete(indexToDelete)
+    simpl.addStep(sum(terms), "Remove term " & $deletedTerm)
+
+  simpl.simplifySum()
 
 proc simplifyFull*(exp: BExp): BExp =
-  return pruneImpliedTerms(simplify(exp))
+  let simpl = newBExpSimplifier(exp)
+  simpl.simplify()
+
+  return simpl.current
 
 when isMainModule:
   import boolexpparser
   import os
-  echo simplifyFull(parse(paramStr(1)))
 
-
+  let simpl = newBExpSimplifier(parse(paramStr(1)))
+  simpl.simplify()
+  simpl.pruneUselessSteps()
+  newFileStream(stdout).write(simpl)
