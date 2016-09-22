daf83a78565f623a80b803d05bc7e81e762645d6
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2016-09-22 15:09:59 -0700

    Use shunting-yard algorithm for parsing
    old parsing file is in boolexpparser_old.nim

--- boolexpparser.nim
+++ boolexpparser.nim
@@ -5,7 +5,7 @@ import strutils
 
 type
   BExpTokenKind = enum
-    BTOParen, BTCParen, BTVar, BTPlus, BTMinus, BTTimes, BTEnd
+    BTOParen, BTCParen, BTVar, BTPlus, BTBar, BTTimes, BTEnd
 
   BExpToken = tuple[
     kind: BExpTokenKind,
@@ -16,15 +16,28 @@ type
     pos: int
     parenDepth: int
 
+  BExpParser = ref object
+    tz: BExpTokenizer
+    outputQueue: seq[BExpToken]
+    opStack: seq[BExpToken]
+
   BExpParseError = object of Exception
 
-proc newTokenizer(text: string): BExpTokenizer =
+proc precedence(token: BExpToken): int =
+  case token.kind:
+    of BTPlus: 3
+    of BTTimes: 2
+    of BTBar: 1
+    else: 0
+
+proc newBExpTokenizer(text: string): BExpTokenizer =
   new result
   result.text = text
   result.pos = 0
   result.parenDepth = 0
 
 proc next(tz: BExpTokenizer, peek = false): BExpToken =
+
   let origPos = tz.pos
   template curch: char = tz.text[tz.pos]
 
@@ -43,8 +56,8 @@ proc next(tz: BExpTokenizer, peek = false): BExpToken =
         result.kind = BTCParen
       of '+':
         result.kind = BTPlus
-      of '-':
-        result.kind = BTMinus
+      of '\'':
+        result.kind = BTBar
       of '*':
         result.kind = BTTimes
       of 'a'..'z', 'A'..'Z':
@@ -59,73 +72,95 @@ proc next(tz: BExpTokenizer, peek = false): BExpToken =
 proc peek(tz: BExpTokenizer): BExpToken =
   tz.next(true)
 
-proc consume(tz: BExpTokenizer, kind: BExpTokenKind): BExpToken =
-  let next = tz.next
-  if next.kind != kind:
-    raise newException(BExpParseError, "Expected token of kind " & $kind & ", but got " & $next.kind & " at pos " & $next.pos)
-  return next
+proc newBExpParser(tz: BExpTokenizer): BExpParser =
+  new result
+  result.tz = tz
+  newSeq(result.outputQueue, 0)
+  newSeq(result.opStack, 0)
 
-proc parseSum(tz: BExpTokenizer): BExp
-proc parseProd(tz: BExpTokenizer): BExp
-proc parseParenExp(tz: BExpTokenizer): BExp
-proc parseSmallExp(tz: BExpTokenizer): BExp
+proc processToken(p: BExpParser, token: BExpToken): bool =
+  result = true
+  case token.kind:
+    of BTVar:
+      p.outputQueue.add(token)
+    of BTPlus, BTTimes, BTBar:
+      while true:
+        let opslen = p.opStack.len
+        if opslen == 0:
+          break
 
-proc parseExp(tz: BExpTokenizer): BExp =
-  result = tz.parseSum()
+        let top = p.opStack[opslen - 1]
 
-proc parseSum(tz: BExpTokenizer): BExp =
-  result = tz.parseProd()
-  var tok = tz.peek()
-  while tok.kind notin {BTCParen, BTEnd}:
-    discard tz.consume(BTPlus)
-    result = result + tz.parseProd()
-    tok = tz.peek()
+        if precedence(top) > 0 and precedence(token) > precedence(top):
+          p.outputQueue.add(p.opStack.pop())
+        else:
+          break
 
-proc parseProd(tz: BExpTokenizer): BExp =
-  template addTerm(term: BExp) =
-    if isNil(result):
-      result = term
-    else:
-      result = result * term
+      p.opStack.add(token)
+    of BTOParen:
+      p.opStack.add(token)
+    of BTCParen:
+      while true:
+        if p.opStack.len == 0:
+          raise newException(BExpParseError, "Unmatched close parenthesis at pos " & $token.pos)
 
-  var tok: BExpToken
-  
-  
-  while true:
-    tok = tz.peek()
-    if tok.kind == BTMinus:
-      discard tz.consume(BTMinus)
-      addTerm(-tz.parseSmallExp())
-    elif tok.kind == BTCParen and tz.parenDepth > 0:
-      return
-    elif tok.kind in {BTPlus, BTEnd}:
-      return
-    else:
-      addTerm(tz.parseSmallExp())
+        let top = p.opStack.pop()
+        if top.kind == BTOParen:
+          break
+        else:
+          p.outputQueue.add(top)
+    of BTEnd:
+      result = false
 
-proc parseParenExp(tz: BExpTokenizer): BExp =
-  discard tz.consume(BTOParen)
-  tz.parenDepth += 1
-  result = tz.parseExp()
-  discard tz.consume(BTCParen)
-  tz.parenDepth -= 1
+  if token.kind in {BTVar, BTBar, BTCParen} and p.tz.peek().kind in {BTVar, BTOParen}:
+    discard p.processToken( (BTTimes, token.pos) )
 
-proc parseSmallExp(tz: BExpTokenizer): BExp =
-  let tok = tz.peek()
-  let image = tz.text[tok.pos] & "" # & "" turns it into a one character string
+proc processNext(p: BExpParser): bool =
+  p.processToken(p.tz.next())
 
-  if tok.kind == BTOParen:
-    return tz.parseParenExp()
-  elif tok.kind == BTVar:
-    discard tz.consume(BTVar)
-    return v(image)
-  else:
-    raise newException(BExpParseError, "Invalid token '" & image & "' at pos " & $tok.pos)
+proc constructResult(p: BExpParser): BExp =
+  var stack: seq[BExp]
+  newSeq(stack, 0)
+
+  for token in p.outputQueue:
+    case token.kind:
+      of BTVar:
+        stack.add(v(p.tz.text[token.pos] & ""))
+      of BTPlus, BTTimes:
+        if stack.len < 2:
+          raise newException(BExpParseError, "Missing operand for operator at pos " & $token.pos)
+
+        let rhs = stack.pop()
+        let lhs = stack.pop()
+
+        if token.kind == BTPlus:
+          stack.add(lhs + rhs)
+        else:
+          stack.add(lhs * rhs)
+      of BTBar:
+        if stack.len < 1:
+          raise newException(BExpParseError, "Missing operand for bar operator at pos " & $token.pos)
+
+        let exp = stack.pop()
+        stack.add(-exp)
+      else:
+        raise newException(BExpParseError, "Invalid token made its way to output stack at pos " & $token.pos)
+
+  return stack.pop()
+
+proc parse*(p: BExpParser): BExp =
+  while p.processNext(): discard
+
+  while p.opStack.len > 0:
+    p.outputQueue.add(p.opStack.pop())
+
+  return p.constructResult()
 
 proc parse*(text: string): BExp =
-  let tz = newTokenizer(text)
-  return tz.parseExp()
+  let tz = newBExpTokenizer(text)
+  let parser = newBExpParser(tz)
+
+  return parser.parse()
 
 when isMainModule:
-  let tz = newTokenizer("(a+b)(c+d)")
-  echo(tz.parseExp())
+  echo parse("a'b+a'c")
